2615b141853ea234ce9442f6158d44caa5e61acb
Melika Hamedani <97775710+Melikahamedani@users.noreply.github.com>
2022-10-20 14:57:02 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,106 @@
+//cyclic link : https://modern-ant-undershirt.cyclic.app/
+
+
+ let HTTP_PORT = process.env.PORT || 8080;
+ let express = require("express");
+ let app = express();
+ let data_prep = require("./data_prep.js");
+
+ 
+
+app.get("/", function(req, res){
+    let resText = "<h2>Declaration:</h2><br>";
+    resText += "<p> I acknowledge the College's integrity policy-and my own integrity- remain in effect whether my work is <br>"
+    resText += "done remotely or onsite. Any test or assignment is an act of trust between me and my instructor, and especially with <br>"
+    resText += "my classmates...even when no one is watching. I declare I will not break that trust.</p><br>"
+    resText += "<p style=background-color:yellow> Name: <span>Melika Hamedani</span></p><br>"
+    resText += "<p style=background-color:yellow> Student Number: <span>175474212</span></p><br>"
+    resText += "<a href = './students.json'> CPA Students</a> <br>";
+    resText += "<a href = './students.json'> Highest GPA </a> <br>"; 
+    resText += "<a href = './students.json'> All Students </a> <br>"; 
+    resText += "<a href = './test3_views/addStudents.html'> Add a new Student </a> <br>"; 
+    res.send(resText);
+});
+
+
+//CPA
+app.get("/CPA", (req,res)=>{
+    data_prep.cpa().then((data)=>{
+        res.json(data);
+    }).catch((reason)=>{
+        res.json({message:reason});
+    });
+});
+
+
+//High GPA
+app.get("/highGPA", (req, res)=>{
+    data_prep.highGPA().then((data)=>{
+        let resText = `<h2> Highest GPA: </h2>
+        <p> Student ID: ${data.studId} </p>
+        <p> Name:  ${data.name} </p>
+        <p> Program: ${data.program} </p>
+        <p> GPA: ${data.gpa} </p> `;
+        res.send(resText);
+    });
+});
+
+
+//get All Students
+app.get("/allStudents", (req, res) => {
+    data_Prep.allStudents().then(data => {
+        res.json(data)
+    }).catch((reason)=>{
+    res.json({message:reason});
+})
+});
+
+//Add student
+app.get("/addStudent", (req, res) => {
+    res.sendFile(path.join(__dirname,"/test3_views/addStudents.html"));
+});
+
+app.post("/addStudent", (req, res) => {
+    data_prep.addStudent(req.body).then(() => {
+        let resText = `<h2> This Student Information </h2>
+        <p> Student ID: ${data.studId} </p>
+        <p> Student Name:  ${data.name} </p>
+        <p> Program: ${data.program} </p>
+        <p> GPA: ${data.gpa} </p> `;
+        resText += "<a href = './students.json'> All Students </a> <br>"; 
+        resText += "<a href = './test3_views/addStudents.html'> Add a new Student </a> <br>"; 
+        res.send(resText);
+    }).catch((reason)=>{
+        res.json({message:reason});
+});
+});
+
+//get Student by Id
+app.get("/student/:studId", (req,res) => {
+    data_prep.getStudnetbyId (req.params.value).then((data) => {
+        res.json({data});
+    }).catch((err) => {
+        res.json({message: err});
+    })
+});
+
+
+//Error 404
+app.get("*", (req, res)=>{
+    res.status(404).send("Error 404: page not found.");
+});
+
+
+
+//PORT
+function onHttpStart() {
+    console.log("Express http server listening on: " + HTTP_PORT);
+};
+
+//prep()
+data_prep.prep()
+    .then(() => {
+        app.listen(HTTP_PORT, () => {
+            console.log(`Express http server listening on ${HTTP_PORT}`)
+        })
+    }).catch(err => console.log(err))
